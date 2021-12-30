75333fc24e48866aa511fce2588eccc61555a2e7
Rozkis <msc_rmaria@accitesz.com>
2021-12-29 21:55:13 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+
+
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+
+const jsonAlumnos = [
+  {
+      key: "1",
+      name: "Alvarez Flores Jesús Manuel"
+  }, {
+      key:"2",
+      name: "Castañeda Vega Gerardo" 
+  }, {
+      key: "3",
+      name: "Castellanos Flores Héctor Gerardo"
+  }, {
+      key: "4",
+      name: "Duarte García Rosa María"
+  }, {
+      key: "5",
+      name: "Martínez Gonzáles Leonardo"
+  }, {
+      key: "6",
+      name:"Ruíz Leo Leonardo"
+  }, {
+      key: "7",
+      name: "Montejano Guerreo Salvador"
+  },{
+      key: "8",
+      name: "Rojas López Luis Fernando"
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [isLoading,setIsLoading] = useState(true);
+
+useEffect(() => {
+  setTimeout(() => {
+      setIsLoading(false);
+}, 4000 )
+}, [])
+
+const renderData = () => {
+  return jsonAlumnos?.map((value) =>  (
+      <div key= {value.key}>
+      <p> {value.name}</p>
+      </div>
+  ))
+}
+return (
+  <div className="App">
+      {isLoading ? <h2>Is Loading...</h2> : renderData()}
+  </div>
+);
+
 }
 
 export default App;
